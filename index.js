b7628285c990e794ddd4833dc2e8f76b56dfbcb0
msaaddev <mrsaadirfan@gmail.com>
2022-04-24 15:48:45 +0500

    📦 NEW: First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Author:
+ * GitHub:
+ * Twitter: https://twitter.com/
+ */
+
+console.log('hello');
+
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+}
+
+const a = 'hellow';
